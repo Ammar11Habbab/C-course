826df93e2d6dd6ce7b76b6b06a7e70d83b378b7b
Ammar <Ammar@AMMAR-PC>
2017-09-15 16:26:29 +0300

    Add  A Comment

--- FirstProgramG/Program.cs
+++ FirstProgramG/Program.cs
@@ -10,6 +10,8 @@ namespace FirstProgramG
     {
         static void Main(string[] args)
         {
+            // this my first program
+
             Console.WriteLine("Hello World");
             Console.ReadKey();
         }
