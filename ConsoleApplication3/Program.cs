fec093ce896a453213d0b593c7dcd7e3ed54de4a
Ammar <Ammar@AMMAR-PC>
2017-09-16 17:34:43 +0300

    impotant slashes

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //\' \" \0 \a \\ \b \n \r \t \v \f
+            //\@
+            string mystring = "ammar said this a good course";
+            string mystring2 = ", yes it's";
+            string mystring3 = mystring + mystring2;
+            Console.WriteLine("{0}", mystring3);
+            Console.ReadKey();
+
+        } 
+    }
+}
---
+++ Littrial Chars/Littrial Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Types_Char_String_Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvar = -100;
+            uint mycar = 1000u;
+            long mydar = 100L;
+            ulong myulong = 200;
+            float myfloat = 1.5F;
+            double mydoub = 105.5d;
+            decimal mydec = 1005.3M;
+            Console.WriteLine(" {0} {1} {2}", myvar, mycar, mydar);
+            Console.WriteLine($"{myfloat},{mydoub},{mydec}");
+            Console.ReadKey();
+
+        }
+    }
+}
