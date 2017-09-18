125b8ab385ab0150cecb1724055d10d058cf657b
Ammar <Ammar@AMMAR-PC>
2017-09-18 22:47:18 +0300

    nested if statment , AHHHHH

---
+++ nested if statments/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_if_statments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myvar1 = 9, myvar2 = 6;
+
+            if (myvar1 == 9)
+            {
+                Console.WriteLine("myvar1 equal 9");
+                if (myvar2 == 7)
+                {
+                    Console.WriteLine("myvar2 equal 7");
+                }
+                else
+                {
+                    Console.WriteLine("myvar2 not equal 7");
+                }
+            }
+            else
+            {
+                Console.WriteLine("myvar1 not equal 9");
+            }
+            Console.ReadKey();
+        }
+    }
+}
