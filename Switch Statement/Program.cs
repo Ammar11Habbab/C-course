c1f16d798b193a541f5e8f4ae3d4d2e679f1a8b4
Ammar <Ammar@AMMAR-PC>
2017-09-19 23:27:30 +0300

    Cases Statements

---
+++ Switch Statement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myanimal = "cat";
+            switch (myanimal)
+            {
+                case "canary":
+                    Console.WriteLine("your animal is canary");
+                    break;
+                case "bird":
+                    Console.WriteLine("your animal is cat");
+                    break;
+                case "dog":
+                    Console.WriteLine("your animal is dog");
+                    break;
+                default:
+                    Console.WriteLine("none of them");
+                    break;
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
