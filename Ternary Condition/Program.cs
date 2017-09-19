6ed93ae9b4b1e8c1ff38a61c355424c78cee0e37
Ammar <Ammar@AMMAR-PC>
2017-09-20 00:48:17 +0300

    Ternary condition

---
+++ Ternary Condition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int myvar = 9;
+            //string myresult = (myvar < 8) ? "less than 8" : "bigger than 8";
+            //Console.WriteLine("{0}", myresult);
+
+            string myname = "Ammar", myresult2;
+            myresult2 = (myname.Equals("saria")) ? "YES" : "NO";
+            Console.WriteLine("{0}", myresult2);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
