b4c7e9608a501f1171b40ad71f663a6daf0a4e99
Ammar <Ammar@AMMAR-PC>
2017-09-20 22:40:09 +0300

    For Loop

---
+++ For Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int mysalary = 1000, total = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                total += mysalary;
+            }
+            Console.WriteLine(" my own salary is {0}", total);
+            Console.ReadKey();
+        }
+    }
+}
