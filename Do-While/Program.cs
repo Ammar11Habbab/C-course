067af8f74b85708df9552d1c3395505e743ec081
Ammar <Ammar@AMMAR-PC>
2017-09-20 21:40:19 +0300

    Do While

---
+++ Do-While/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double mysalary = 1000.20, total = 20.20;
+            int i = 0;
+            do
+            {
+
+                total += mysalary;
+                i++;
+                Console.WriteLine(" my value equal {0}", i);
+
+
+            }
+            while (i < 10);
+            Console.WriteLine(" my salary equal {0}", total);
+            Console.ReadKey();
+
+
+        }
+    }
+}
