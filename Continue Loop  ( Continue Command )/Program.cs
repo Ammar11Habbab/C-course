5da8e26272256cd425a225fed902b548d692fab0
Ammar <Ammar@AMMAR-PC>
2017-09-21 13:42:03 +0300

    Continue Loop (Continue Command)

---
+++ Continue Loop  ( Continue Command )/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue_Loop____Continue_Command__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Ammar");
+                if (i > 2)
+                    continue;
+                for (int j = 0; j <= 3; j++)
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
