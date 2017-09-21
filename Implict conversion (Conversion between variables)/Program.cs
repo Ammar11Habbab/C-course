b19e4978fff438229fe1b58ae3a21f0546d07461
Ammar <Ammar@AMMAR-PC>
2017-09-21 15:39:30 +0300

    Implict conversion (Conversion between variables)

---
+++ Implict conversion (Conversion between variables)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implict_conversion__Conversion_between_variable_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvar1 = 1000;
+            float myvar2 = myvar1;
+            Console.WriteLine($"{myvar2}");
+            char myvar3 = 'G';
+            int myvar4 = myvar3;
+            Console.WriteLine($"{myvar4}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
