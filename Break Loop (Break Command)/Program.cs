6b707a7288ed53369a723d10fd8f55f023d9da9f
Ammar <Ammar@AMMAR-PC>
2017-09-21 12:34:56 +0300

    Break Loop (Breaking loop command stop the loop at certain value you choose)

---
+++ Break Loop (Break Command)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop__Break_Command_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Ammar");
+                if (i > 2)
+                    break;
+                for(int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("Hello Syria");
+                }
+}
+            Console.ReadKey();
+        }
+    }
+}
