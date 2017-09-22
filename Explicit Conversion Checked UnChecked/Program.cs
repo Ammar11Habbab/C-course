acdac939ffcb94a3af842e9c71bc09f6e45f2abf
Ammar <Ammar@AMMAR-PC>
2017-09-22 11:44:41 +0300

    Explicit Conversion ( Checked and UnChecked Command)

---
+++ Explicit Conversion Checked UnChecked/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion_Checked_UnChecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myintvar1 = 350;
+            byte mybytevar1;
+
+            mybytevar1 = checked((byte)myintvar1);
+            // we use checked to check if there is an overflow
+            // unchecked we use it if the setting in visual studio to always check overflow used
+            Console.WriteLine($"{myintvar1} \n {mybytevar1}");
+            Console.ReadKey();
+        }
+    }
+}
