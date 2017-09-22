d3bb6eb0462a2b9fb746bd99cb0eb60d0205478f
Ammar <Ammar@AMMAR-PC>
2017-09-22 10:47:22 +0300

    Explicit Conversion (Between Variables that cant do the implicit conversion ) and what is overflow state and how to avoid it

---
+++ Explicit Conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // carefull not to make a mistake and get into overflow state
+            int myintvar = 7;
+            byte mybytevar;
+            mybytevar = (byte)myintvar;
+            Console.WriteLine($"my int variable = {myintvar}" + "\n");
+            Console.WriteLine($"my bytevar = {mybytevar}" + "\n");
+            Console.ReadKey();
+        }
+    }
+}
