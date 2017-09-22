50cd209b774e19b82ef54eaaec4d88e65bdd50d6
Ammar <Ammar@AMMAR-PC>
2017-09-22 17:59:12 +0300

    Input Command to Write on Console Using console.readline and read

---
+++ Input Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname;
+            int myvar1, myvar2;
+            Console.WriteLine("\n Enter Your Name \n");
+            myname = Console.ReadLine();
+            Console.WriteLine("\n Enter Your Number \n");
+            myvar1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Enter Your Char \n");
+            // read will allow you to put 1 char and it will give you the asky code number of it BUT remember you can 
+            // remember you can change that using conversions
+            myvar2 = Console.Read();
+            Console.WriteLine($"\n Your Name is {myname} \n You Number is {myvar1} \n Your Char ASKY Code is {myvar2}");
+            Console.ReadKey();
+        }
+    }
+}
