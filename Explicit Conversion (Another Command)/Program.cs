cbb9130dc2ed6cf0b4a070bbda6b03b74f72efbf
Ammar <Ammar@AMMAR-PC>
2017-09-22 14:26:53 +0300

    Explicit Conversion (Another Command) and Substring command to cut numbers

---
+++ Explicit Conversion (Another Command)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion__Another_Command_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myvar1 = 2500.20d;
+            string mystr1, mystr2;
+            // to make an explicit conversion in another command and you can put mystr1 and put conver to integer is should be the same varibles
+            mystr1 = Convert.ToString(myvar1);
+            // we use it to cut from numbers by substring command mystr2 = mystr1.Substring(0, 2);
+            mystr2 = mystr1.Substring(0, 2);
+            Console.WriteLine($"{myvar1}\n{mystr1}\n{mystr2}");
+            Console.ReadKey();
+        }
+    }
+}
