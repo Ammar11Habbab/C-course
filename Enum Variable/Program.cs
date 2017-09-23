62961dbcc4ab37be2abcff7fef4e0476391c4832
Ammar <Ammar@AMMAR-PC>
2017-09-23 14:21:12 +0300

    Enum Variable

---
+++ Enum Variable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Variable
+{
+    class Program
+    {
+        enum humansex : sbyte
+        {
+            male = 1,
+            female = 2
+        }
+
+        static void Main(string[] args)
+        {
+            sbyte mysbyte;
+            string gender;
+            humansex mystrhuman = humansex.male;
+            Console.WriteLine($" the gender is {mystrhuman}" + "\n");
+            mysbyte = (sbyte)mystrhuman;
+            Console.WriteLine($" my index gender number is {mysbyte}" + "\n");
+            gender = Convert.ToString(mystrhuman);
+            Console.WriteLine($" sex is {gender}" + "\n");
+            Console.ReadKey();
+        }   
+    }
+}
