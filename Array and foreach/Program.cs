4497ae1d14f7efbe73ed8173ff217b785189ccdf
Ammar <Ammar@AMMAR-PC>
2017-09-25 00:25:27 +0300

    Array and Foreach command that is safer that for

---
+++ Array and foreach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_and_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // use foreach is safer than for because in for you can make an index mistake
+            int[] mynumbers = { 1, 5, 7, 9, 3 };
+            foreach (int item in mynumbers)
+            {
+                Console.WriteLine("My Number is " + $"{item}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
