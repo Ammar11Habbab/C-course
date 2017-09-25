6fa74f8a7cdca00ce757046d3873e33e3ac955fc
Ammar <Ammar@AMMAR-PC>
2017-09-25 19:48:53 +0300

    when to use foreach and when to not use it and instead using forloop

---
+++ For array vs foreach array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_array_vs_foreach_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] myfullname = { "Ammar", "Yaser", "Habbab" };
+            int myarraylength = myfullname.Length;
+            for (int i = 0; i < myarraylength; i++)
+                {
+             myfullname[2] = "Hbab";
+            // we use forloop if we want to change a value in the array
+            Console.Write($"{myfullname[i]}" + " ");
+
+                  }
+            Console.WriteLine($"\n{myarraylength}");
+            foreach (string myname in myfullname)
+            Console.Write($"{myname}" + " ");
+            // we cant use foreach if we want to change inside the loop an array value
+            Console.ReadKey();
+        }
+    }
+}
