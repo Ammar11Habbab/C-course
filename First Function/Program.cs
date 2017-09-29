6239384de30b87cf8f48a1c0281b8161dc2615d2
Ammar <Ammar@AMMAR-PC>
2017-09-29 23:44:09 +0300

    First Function

---
+++ First Function/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Function
+{
+    class Program
+    {
+        static void nameprint()
+        {
+            Console.WriteLine( "Hello Ammar" );
+        }
+        static void Main(string[] args)
+        {
+            nameprint();
+            Console.ReadKey();
+        }
+    }
+}
