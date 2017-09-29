c7b57fad5405e1ccc092de2e8805fcb94ca17350
Ammar <Ammar@AMMAR-PC>
2017-09-29 16:17:49 +0300

    Split Function with String Type

---
+++ Split Function with String Type/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_Function_with_String_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring;
+            char[] seperator = { 'u',' ' };
+            mystring = Console.ReadLine();
+            string[] mynewwords;
+            mynewwords = mystring.Split(seperator);
+            foreach (string word in mynewwords)
+            {
+                Console.WriteLine($"{word}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
