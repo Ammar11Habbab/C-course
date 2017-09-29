070734d053acf5a32688088a34c6f95cf391adaa
Ammar <Ammar@AMMAR-PC>
2017-09-29 12:50:41 +0300

    ToUpper Vs ToLower and Trim Functions

---
+++ ToUpper Vs ToLower and Trim Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToUpper_Vs_ToLower_and_Trim_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring, mystring2;
+            char[] mytrimchar = { 'h', 'e', 'i', ' ' };
+            Console.WriteLine("pleae enter your string");
+            mystring = Console.ReadLine();
+            mystring = mystring.ToLower();
+            mystring2 = mystring.Trim(mytrimchar);
+            Console.WriteLine($"{mystring2}");
+            Console.ReadKey();
+            // trim تخلصنا من الفراغات التي قبل اول كلمة وبعد اخر كلمة وايضا تحذف الحروف التي نعرفها كما فوق
+            // tolower تحول جميع الحروف الى صغيرة 
+            // toupper تحول جميع الحروف الى كبيرة
+        }
+    }
+}
