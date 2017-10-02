9cc9bb59ab0488f53e4b3a8a96d23f1eff0a8156
Ammar <Ammar@AMMAR-PC>
2017-10-02 19:54:28 +0300

    Reference Values

---
+++ Reference values/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reference_values
+{
+    class Program
+    {
+        static void multiplynum(ref int myvar)
+        {
+            myvar *= 3;
+            Console.WriteLine($" my number is {myvar}");
+        }
+        static void Main(string[] args)
+        {
+            int mynumber = 5;
+            Console.WriteLine($" my number is {mynumber}");
+            multiplynum(ref mynumber);
+            Console.WriteLine($" my number is {mynumber}");
+            Console.ReadKey();
+            // نضع ref لان دالة المين لا تغير قيمة المتغير مع اننا غيرناها في الدالة 
+        }
+    }
+}
