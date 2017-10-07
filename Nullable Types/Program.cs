cc82710f97e20dd85aa5256318a232eee2ff8283
Ammar <Ammar@AMMAR-PC>
2017-10-07 20:34:02 +0300

    Nullable Types

---
+++ Nullable Types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Nullable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = null;
+            int? myint = null;
+            WriteLine($"{mystring}");
+            WriteLine($"{myint}");
+            ReadKey();
+        }
+    }
+}
