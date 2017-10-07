0ce0a16b28fbb1ab89cf81599a136a8ceffca0a4
Ammar <Ammar@AMMAR-PC>
2017-10-07 10:54:18 +0300

    Using Namespaces in Practical

---
+++ Using Namespaces in Practical/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Console;
+using OB = Brothers.MyBrothers.OldBrothers;
+using YB = Brothers.MyBrothers.YoungBrothers;
+
+
+namespace Using_Namespaces_in_Practical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Those Are My Famliy");
+            OB.print();
+            YB.print();
+            ReadKey();
+        }
+    }
+}
+namespace Brothers
+{
+    namespace MyBrothers
+    {
+        class OldBrothers
+        {
+            public static void print()
+            {
+                Write("Dina " + "And " + "Zeina" );
+
+            }
+
+        }
+
+    }
+    namespace MyBrothers
+    {
+        class YoungBrothers
+
+        {
+            public static void print()
+            {
+                Write("\n" + "Saria " + "And " + "Ibrahim ");
+            }
+
+        }
+
+    }
+}
