417086e322dc55abbedd6e1c9d2b87b7089e796f
Ammar <Ammar@AMMAR-PC>
2017-10-10 19:55:23 +0300

    Optional Parameters

---
+++ Optional Parameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+        public static void PrintValues(string name = "Ammar Habbab", int Number = 8)
+        {
+            Console.WriteLine($"My Name is {name} And My Number is {Number} ");
+
+        }
+        static void Main(string[] args)
+        {
+            string name = "Maison";
+            int Number = 9;
+            PrintValues();
+            PrintValues(name);
+            PrintValues(name, Number);
+            PrintValues(name: "Yaser", Number: 4);
+            Console.ReadKey();
+
+        }
+    }
+}
