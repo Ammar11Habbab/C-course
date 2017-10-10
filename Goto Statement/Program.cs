323d84d9c50219e9b1333791ceb3c312b5350ed6
Ammar <Ammar@AMMAR-PC>
2017-10-10 13:37:23 +0300

    Goto Statement

---
+++ Goto Statement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goto_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+        myLable:
+            if (counter < 10)
+            {
+                counter++;
+                goto myLable;
+            }
+            else
+                Console.WriteLine("Done");
+            Console.WriteLine(counter);
+            Console.ReadKey();
+
+            // P.S we dont use goto alot because its kind of slow and old
+            // and because there is forloop
+        }
+    }
+}
